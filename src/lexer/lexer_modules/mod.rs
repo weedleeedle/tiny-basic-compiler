0350607c183b72033449a51939ccc07bc5d73630
Weedleeedle <11594471+ViperZer0@users.noreply.github.com>
2025-11-28 17:56:19 -0800

    Added NewlineLexerModule

--- src/lexer/lexer_modules/mod.rs
+++ src/lexer/lexer_modules/mod.rs
@@ -4,12 +4,14 @@ mod variable_lexer_module;
 mod number_lexer_module;
 mod symbol_lexer_module;
 mod string_lexer_module;
+mod newline_lexer_module;
 
 pub use keyword_lexer_module::KeywordLexerModule;
 pub use variable_lexer_module::VariableLexerModule;
 pub use number_lexer_module::NumberLexerModule;
 pub use symbol_lexer_module::SymbolLexerModule;
 pub use string_lexer_module::StringLexerModule;
+pub use newline_lexer_module::NewlineLexerModule;
 
 /// Gets the first word (up to the first unicode whitespace).
 /// Returns [None] if the string is empty or all whitespace.
---
+++ src/lexer/lexer_modules/newline_lexer_module.rs
@@ -0,0 +1,54 @@
+//! Lexer module that parses newlines.
+
+use crate::lexer::{LexerModule, LexerModuleResult, LexerModuleSuccessResult, Token};
+
+pub struct NewlineLexerModule();
+
+impl LexerModule for NewlineLexerModule
+{
+    fn parse_stream<'a>(&mut self, stream: &'a str) -> LexerModuleResult<'a>
+    {
+        if stream.starts_with('\n')
+        {
+            return LexerModuleResult::TokenSuccess(
+                LexerModuleSuccessResult
+                {
+                    remainder: &stream[1..],
+                    token: Token::NewLine,
+                }
+            );
+        }
+        else
+        {
+            return LexerModuleResult::TokenIgnored
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests
+{
+    use super::*;
+
+    #[test]
+    fn test_newline_lexer_module()
+    {
+        let mut lexer_module = NewlineLexerModule();
+        let input_stream = "\nInput";
+        let token = lexer_module.parse_stream(input_stream);
+        assert!(token.is_success());
+        let token = token.unwrap();
+        assert_eq!(token.token, Token::NewLine);
+        assert_eq!(token.remainder, "Input");
+    }
+
+    #[test]
+    fn test_newline_lexer_module_ignores_non_newline_char()
+    {
+        let mut lexer_module = NewlineLexerModule();
+        let input_stream = "Hi :)";
+        let token = lexer_module.parse_stream(input_stream);
+        assert!(token.is_ignored());
+    }
+}
+
